5383fa9d25baf2074688c9169308b2dda82c34bd
Kadjoassohoun <32166865+Kadjoassohoun@users.noreply.github.com>
2018-10-10 21:57:07 +0200

    Add files via upload

---
+++ TD1_Poa/src/fr/eliseeassohoun/miage/poa/shapes/impl/Point3D.java
@@ -0,0 +1,16 @@
+package fr.eliseeassohoun.miage.poa.shapes.impl;
+
+public class Point3D extends Point2D {
+
+    private double z;
+
+    public Point3D()
+    {
+        this.z = 0;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+}
