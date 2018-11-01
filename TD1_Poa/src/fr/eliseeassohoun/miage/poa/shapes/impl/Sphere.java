ff2bff8416642d6c23e34012619bf66f94d5ef6f
Kadjoassohoun <32166865+Kadjoassohoun@users.noreply.github.com>
2018-11-01 23:31:08 +0100

    Update Sphere.java

--- TD1_Poa/src/fr/eliseeassohoun/miage/poa/shapes/impl/Sphere.java
+++ TD1_Poa/src/fr/eliseeassohoun/miage/poa/shapes/impl/Sphere.java
@@ -2,21 +2,31 @@ package fr.eliseeassohoun.miage.poa.shapes.impl;
 
 public class Sphere extends Shape3D {
 
+    public static final double PI = 3.14159265358979323846;
     private double radius;
 
-    public Sphere( double ra)
+    public Sphere(double aRadius)
     {
-        this.radius = ra;
+        this.radius = aRadius;
+    }
+
+    public Sphere(Point3D refPoint) {
+        super(refPoint);
     }
 
     public double getRadius()
     {
-        return radius;
+        return this.radius;
     }
 
-    public void setRadius( double radius)
+    public void setRadius(double radius)
     {
         this.radius = radius;
+    }
+
+    public double volume()
+    {
+        return (4.0 / 3.0) * PI * Math.pow(radius, 3);
     }
 
 }
