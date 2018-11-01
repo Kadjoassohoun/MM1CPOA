8ecd647405d4fc9e042e477c5f70abffe7173a16
Kadjoassohoun <32166865+Kadjoassohoun@users.noreply.github.com>
2018-11-01 23:28:04 +0100

    Update Point3D.java

--- TD1_Poa/src/fr/eliseeassohoun/miage/poa/shapes/impl/Point3D.java
+++ TD1_Poa/src/fr/eliseeassohoun/miage/poa/shapes/impl/Point3D.java
@@ -6,11 +6,31 @@ public class Point3D extends Point2D {
 
     public Point3D()
     {
-        this.z = 0;
+        this.z = 0.0;
     }
 
     public double getZ() {
         return z;
     }
 
+    @Override
+    public void translate(double xs, double ys, double zs)
+    {
+        super.translate(double xs, double ys);
+        this.z = this.z + zs;
+    }
+
+    @Override
+    public double distance (Point3D dist)
+    {
+        double dz = getZ()-dist.z;
+        double dx = getX()-dist.x;
+        double dy = getY()-dist.y;
+
+
+        return Math.sqrt((dx * dx * dx) + (dy * dy * dy) + (dz * dz dz);)
+
+    }
+
 }
+
