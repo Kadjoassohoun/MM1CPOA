a13887190345c18571996c5ccc5a1dd450d46cfc
Kadjoassohoun <32166865+Kadjoassohoun@users.noreply.github.com>
2018-11-01 23:33:14 +0100

    Add files via upload

---
+++ TD1_Poa/src/fr/eliseeassohoun/miage/poa/shapes/api/Translatable3D.java
@@ -0,0 +1,9 @@
+package fr.eliseeassohoun.miage.poa.shapes.api;
+
+import fr.eliseeassohoun.miage.poa.shapes.impl.Point3D;
+
+public interface Translatable3D extends Translatable2D  {
+
+    Point3D getRefPoint();
+
+}
